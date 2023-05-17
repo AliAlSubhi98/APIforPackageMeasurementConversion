251928803dac4f31fce69c9db139fde06e252f06
Ali AlSubhi <alialsubhi1998@gmail.com>
2023-05-17 15:52:59 +0400

    trying to do DaniyalTask

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.io.Console;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,20 +8,39 @@ import java.util.List;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        List<Integer> outputs = new ArrayList<>();
+        List<Integer> valuesArr = new ArrayList<>();
 
-        String word = "abbcc";
+        String word = "azbcaaa";
         int value = 0;
         for (int i = 0; i < word.length(); i++) {
             char letter = word.charAt(i);
             value = (int) letter - 96;
+            if(letter == '_'){
+                value = 0;
+            }
+            //System.out.println(value);
+            valuesArr.add(value);
+        } // here I convert the characters into numbers
 
-            System.out.println(value);
-            outputs.add(value);
+        List<Integer> encodedArr = new ArrayList<>();
+        int currentIndex = 0;
 
+        while (currentIndex < valuesArr.size()) {
+            int num = valuesArr.get(currentIndex);
+
+            int sum = 0;
+
+            for (int i = currentIndex + 1; i <= currentIndex + num; i++) {
+                if (i < valuesArr.size()) {
+                    sum += valuesArr.get(i);
+                }
+            }
+
+            encodedArr.add(sum);
+            currentIndex += (num + 1);
         }
 
-        System.err.println(outputs);
+        System.err.println("Encoded Chars to numbers : " + encodedArr);
 
     }
 }
